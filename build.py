3b733307f48c638c1d3731c518dc38a0a33b39ec
Michael Gruber <aelgru@gmail.com>
2013-02-21 16:48:32 +0100

    using new module pybuilder

--- build.py
+++ build.py
@@ -13,7 +13,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from pythonbuilder.core import init, use_plugin, Author
+from pybuilder.core import init, use_plugin, Author
 
 use_plugin("python.core")
 
