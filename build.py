b3e74343231793750ca204a5a49cac95cdd58b7a
mriehl <maximilien.riehl@gmail.com>
2013-10-29 13:06:22 +0100

    releasing 0.9.13

--- build.py
+++ build.py
@@ -49,7 +49,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            Author("Udo Juettner", "udo.juettner@gmail.com")]
 url = "http://pybuilder.github.com"
 license = "Apache License"
-version = "0.9.12"
+version = "0.9.13"
 
 default_task = ["analyze", "publish"]
 
