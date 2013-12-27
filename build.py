05366393f8d9bcfc031e0251945ba32ea86186c7
mriehl <max@riehl.io>
2013-12-27 11:27:31 +0100

    fixed #30 by removing disturbing distribute bootstrapping script from mockito sources. I have no idea if it's worth it to try to upstream my fix because the last mockito change is from 2010

--- build.py
+++ build.py
@@ -55,7 +55,7 @@ default_task = ["analyze", "publish"]
 
 @init
 def initialize(project):
-    project.build_depends_on("mockito")
+    project.build_depends_on("mockito-without-hardcoded-distribute-version")
     project.build_depends_on("mock")
     project.build_depends_on("pyassert")
     project.build_depends_on("wheel")
