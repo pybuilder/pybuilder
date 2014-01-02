b041e3b63361158bb4380d3fdcb5af7f71de4c6f
aelgru <aelgru@gmail.com>
2014-01-02 18:35:18 +0100

    Reducing string since the string is not a message but a module name.

--- build.py
+++ build.py
@@ -112,8 +112,7 @@ def pdoc_generate(project, logger):
         import pdoc
         logger.debug("pdoc is installed in version %s" % pdoc.__version__)
     except ImportError:
-        raise MissingPrerequisiteException(
-            "Missing the pdoc module, please install it with pip", caller="pdoc generator task")
+        raise MissingPrerequisiteException("pdoc", caller="pdoc generator task")
     logger.info("Generating pdoc documentation")
     source_directory = project.get_property("dir_source_main_python")
     subprocess.check_call(
