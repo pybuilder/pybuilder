cdb12a6de45e9b8e0db6c23a7224d9d8af437046
mriehl <max@riehl.io>
2014-01-02 13:46:55 +0100

    adding docstring

--- src/main/python/pybuilder/utils.py
+++ src/main/python/pybuilder/utils.py
@@ -13,6 +13,10 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+"""
+    The PyBuilder utils module.
+    Provides generic utilities that can be used by plugins.
+"""
 
 import json
 import os
