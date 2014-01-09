f77ae3b55e0103d6434a6af912cc5991885a34a0
aelgru <aelgru@gmail.com>
2014-01-09 01:04:34 +0100

    Fixing homepage url: pybuilder.github.io

--- build.py
+++ build.py
@@ -53,7 +53,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            Author("Maximilien Riehl", "max@riehl.io"),
            Author("Michael Gruber", "aelgru@gmail.com"),
            Author("Udo Juettner", "udo.juettner@gmail.com")]
-url = "http://pybuilder.github.com"
+url = "http://pybuilder.github.io"
 license = "Apache License"
 version = "0.9.18"
 
