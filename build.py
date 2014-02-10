140fb6be0e92a2a8261e346ff5810daaf823fdea
mriehl <max@riehl.io>
2014-02-10 09:10:58 +0100

    now out of alpha

--- build.py
+++ build.py
@@ -97,7 +97,7 @@ def initialize(project):
                          'Programming Language :: Python :: 3',
                          'Programming Language :: Python :: 3.2',
                          'Programming Language :: Python :: 3.3',
-                         'Development Status :: 3 - Alpha',
+                         'Development Status :: 4 - Beta',
                          'Environment :: Console',
                          'Intended Audience :: Developers',
                          'License :: OSI Approved :: Apache Software License',
