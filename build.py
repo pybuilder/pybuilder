143ee9be7e51bbd139a9cbe83bc00326241fe898
Valentin Haenel <valentin.haenel@gmx.de>
2014-03-31 15:04:31 +0200

    try fixing version comparison again

--- build.py
+++ build.py
@@ -31,8 +31,9 @@ use_plugin("python.unittest")
 use_plugin("python.integrationtest")
 use_plugin("python.flake8")
 use_plugin("python.frosted")
-if not (sys.version_info.major == 3 and sys.version_info.minor == 2):
+if not sys.version_info[0:2] == (3,2):
     use_plugin("python.cram")
+
 use_plugin("python.pydev")
 use_plugin("python.pycharm")
 use_plugin("python.pytddmon")
