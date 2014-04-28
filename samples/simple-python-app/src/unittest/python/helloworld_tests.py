ac2fe480e8508a654fc555685baf333b399eb74b
mriehl <max@riehl.io>
2014-04-28 16:49:26 +0200

    optimized imports in sample

--- samples/simple-python-app/src/unittest/python/helloworld_tests.py
+++ samples/simple-python-app/src/unittest/python/helloworld_tests.py
@@ -13,9 +13,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from mockito import mock, when, any, unstub, verify
 import unittest
-import sys
+
+from mockito import mock, verify
 
 from helloworld import helloworld
 
