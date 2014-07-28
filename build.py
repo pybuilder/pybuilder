d66e0b05f2b61ff0dec79765077794250778130e
mriehl <max@riehl.io>
2014-07-28 14:27:09 +0200

    v0.10.31

--- build.py
+++ build.py
@@ -69,7 +69,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            Author("Udo Juettner", "udo.juettner@gmail.com")]
 url = "http://pybuilder.github.io"
 license = "Apache License"
-version = "0.10.30"
+version = "0.10.31"
 
 default_task = ["analyze", "publish"]
 
