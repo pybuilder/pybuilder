91c1ae360840c241f42c700f80149811a0b6ddb1
Thomas Prebble <thomas.prebble@gmail.com>
2015-01-01 04:52:05 +1300

    Adding test for sphinx build command creation

--- src/unittest/python/plugins/python/sphinx_plugin_tests.py
+++ src/unittest/python/plugins/python/sphinx_plugin_tests.py
@@ -20,7 +20,9 @@ from unittest import TestCase
 from mock import Mock, patch
 from logging import Logger
 
+from pybuilder.core import Project
 from pybuilder.plugins.python.sphinx_plugin import assert_sphinx_is_available
+from pybuilder.plugins.python.sphinx_plugin import get_sphinx_build_command
 
 
 class CheckSphinxAvailableTests(TestCase):
@@ -34,3 +36,19 @@ class CheckSphinxAvailableTests(TestCase):
 
         expected_command_line = ['sphinx-build', '--version']
         mock_assert_can_execute.assert_called_with(expected_command_line, 'sphinx', 'plugin python.sphinx')
+
+
+class SphinxBuildCommandTests(TestCase):
+
+    def test_should_generate_sphinx_build_command_per_project_properties(self):
+        project = Project('basedir')
+
+        project.set_property("sphinx_builder", "html")
+        project.set_property("sphinx_config_path", "docs/")
+        project.set_property("sphinx_source_dir", "docs/")
+        project.set_property("sphinx_output_dir", "docs/_build/")
+
+        sphinx_build_command = get_sphinx_build_command(project)
+
+        self.assertEqual(sphinx_build_command,
+                         "sphinx-build -b html -c basedir/docs/ basedir/docs/ basedir/docs/_build/")
