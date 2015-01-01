57516fba079e7249f6e99528281529a8e68c0695
mriehl <max@riehl.io>
2015-01-01 10:37:40 +0000

    s/2014/2015/g

--- src/integrationtest/python/should_list_multiple_tasks_for_project_using_core_plugin_tests.py
+++ src/integrationtest/python/should_list_multiple_tasks_for_project_using_core_plugin_tests.py
@@ -2,7 +2,7 @@
 #
 #   This file is part of PyBuilder
 #
-#   Copyright 2011-2014 PyBuilder Team
+#   Copyright 2011-2015 PyBuilder Team
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
