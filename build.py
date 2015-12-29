9b962d9b2fec3fbb388176a2848a9e973219e87c
Valentin Haenel <valentin@haenel.co>
2015-12-29 08:15:14 +0100

    bump to next dev version

--- build.py
+++ build.py
@@ -73,7 +73,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            ]
 url = "http://pybuilder.github.io"
 license = "Apache License"
-version = "0.11.3"
+version = "0.11.4.dev"
 
 default_task = ["analyze", "publish"]
 
