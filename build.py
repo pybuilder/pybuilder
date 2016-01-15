9c5831c648f4ec6114906e92609b56ed49db08f7
mriehl <max@riehl.io>
2016-01-15 13:23:29 +0100

    0.11.5 development version

--- build.py
+++ build.py
@@ -74,7 +74,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            ]
 url = "http://pybuilder.github.io"
 license = "Apache License"
-version = "0.11.4"
+version = "0.11.5.dev"
 
 default_task = ["analyze", "publish"]
 
