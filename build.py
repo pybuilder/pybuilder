bc3b6702dbe70cf2ba79af2d6ac671d4e1b6df0e
mriehl <max@flatmap.ninja>
2016-03-24 12:48:45 +0100

    begin v0.11.9 development

--- build.py
+++ build.py
@@ -77,7 +77,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            ]
 url = "http://pybuilder.github.io"
 license = "Apache License"
-version = "0.11.8"
+version = "0.11.9.dev"
 
 default_task = ["analyze", "publish"]
 
