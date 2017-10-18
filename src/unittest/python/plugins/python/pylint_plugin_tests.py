dfc9b096257a43fb23b912982a4580322536bd89
michalfrak <michal.frak@gmail.com>
2017-10-18 09:07:19 +0200

    [PYB_01] Code review - execution separated from assertion

--- src/unittest/python/plugins/python/pylint_plugin_tests.py
+++ src/unittest/python/plugins/python/pylint_plugin_tests.py
@@ -37,60 +37,61 @@ class PylintPluginTests(TestCase):
         self.mock_logger = Mock(Logger)
 
     @patch('pybuilder.plugins.python.pylint_plugin.assert_can_execute')
-    def test_should_check_that_pylint_can_be_executed(self, mock_assert_can_execute, execute_tool):
+    def test_should_check_that_pylint_can_be_executed(self, mock_assert_can_execute, mock_execute_tool):
         check_pylint_availability(self.mock_logger)
         expected_command_line = ('pylint',)
         mock_assert_can_execute.assert_called_with(expected_command_line, 'pylint', 'plugin python.pylint')
 
-    def test_should_run_pylint_with_default_options(self, execute_tool):
-        self._make_temp_test_file('E:476, 8: Error message')
-        self._run_execute_tool(execute_tool).assert_called_with(
-            self.project, "pylint", ["pylint"] + DEFAULT_PYLINT_OPTIONS, True
-        )
+    def test_should_run_pylint_with_default_options(self, mock_execute_tool):
+        self._create_pylint_log_file('E:476, 8: Error message')
+        execute_tool = self._run_execute_tool(mock_execute_tool)
+        execute_tool.assert_called_with(self.project, "pylint", ["pylint"] + DEFAULT_PYLINT_OPTIONS, True)
 
-    def test_should_run_pylint_with_custom_options(self, execute_tool):
-        self._make_temp_test_file('E:476, 8: Error message')
+    def test_should_run_pylint_with_custom_options(self, mock_execute_tool):
+        self._create_pylint_log_file('E:476, 8: Error message')
         self.project.set_property("pylint_options", ["--test", "-f", "--x=y"])
-        self._run_execute_tool(execute_tool).assert_called_with(
-            self.project, "pylint", ["pylint", "--test", "-f", "--x=y"], True
-        )
+        execute_tool = self._run_execute_tool(mock_execute_tool)
+        execute_tool.assert_called_with(self.project, "pylint", ["pylint", "--test", "-f", "--x=y"], True)
 
-    def test_should_show_error_message_in_pyb_logs(self, execute_tool):
-        self._make_temp_test_file('E:476, 8: Error message')
-        self._run_execute_tool(execute_tool)
+    def test_should_show_error_message_in_pyb_logs(self, mock_execute_tool):
+        self._create_pylint_log_file('E:476, 8: Error message')
+        self._run_execute_tool(mock_execute_tool)
         self.mock_logger.error.assert_called_with('Pylint: Module : E:476, 8: Error message')
 
-    def test_should_show_warning_message_in_pyb_logs(self, execute_tool):
-        self._make_temp_test_file('W:2476, 8: Warning message')
+    def test_should_show_warning_message_in_pyb_logs(self, mock_execute_tool):
+        self._create_pylint_log_file('W:2476, 8: Warning message')
         self.project.set_property('pylint_show_warning_messages', True)
-        self._run_execute_tool(execute_tool)
+        self._run_execute_tool(mock_execute_tool)
         self.mock_logger.warn.assert_called_with('Pylint: Module : W:2476, 8: Warning message')
 
-    def test_should_break_build_on_errors(self, execute_tool):
-        self._make_temp_test_file('E:2476, 8: Warning message')
+    def test_should_break_build_on_errors(self, mock_execute_tool):
+        self._create_pylint_log_file('E:2476, 8: Warning message')
         self.project.set_property('pylint_break_build_on_errors', True)
-        with self.assertRaises(BuildFailedException):
-            self._run_execute_tool(execute_tool)
+        expected_message = 'Pylint: Building failed due to 1 errors or fatal errors'
+        with self.assertRaisesRegexp(BuildFailedException, expected_message):
+            self._run_execute_tool(mock_execute_tool)
 
-    def test_should_break_build_on_too_low_pylint_score(self, execute_tool):
-        self._make_temp_test_file('Your code has been rated at 4.60/10 (previous run: 4.60/10, +0.00)')
+    def test_should_break_build_on_too_low_pylint_score(self, mock_execute_tool):
+        self._create_pylint_log_file('Your code has been rated at 4.60/10 (previous run: 4.60/10, +0.00)')
         self.project.set_property('pylint_score_threshold', 5.0)
-        with self.assertRaises(BuildFailedException):
-            self._run_execute_tool(execute_tool)
+        expected_message = 'Pylint: Building failed due to Pylint score\(4.6\) less then expected\(5.0\)'
+        with self.assertRaisesRegexp(BuildFailedException, expected_message):
+            self._run_execute_tool(mock_execute_tool)
 
-    def test_should_break_build_on_too_high_pylint_score_decrease(self, execute_tool):
-        self._make_temp_test_file('Your code has been rated at 4.60/10 (previous run: 6.60/10, -2.00)')
+    def test_should_break_build_on_too_high_pylint_score_decrease(self, mock_execute_tool):
+        self._create_pylint_log_file('Your code has been rated at 4.60/10 (previous run: 6.60/10, -2.00)')
         self.project.set_property('pylint_score_change_threshold', -1.0)
-        with self.assertRaises(BuildFailedException):
-            self._run_execute_tool(execute_tool)
+        expected_message = 'Pylint: Building failed due to Pylint score decrease\(-2.0\) higher then allowed\(-1.0\)'
+        with self.assertRaisesRegexp(BuildFailedException, expected_message):
+            self._run_execute_tool(mock_execute_tool)
 
-    def _make_temp_test_file(self, test_file_content):
+    def _create_pylint_log_file(self, test_file_content):
         self.temp_file = NamedTemporaryFile()
         with open(self.temp_file.name, 'w') as f:
             f.write(test_file_content)
         return self.temp_file
 
-    def _run_execute_tool(self, execute_tool):
-        execute_tool.return_value = (0, self.temp_file.name)
+    def _run_execute_tool(self, mock_execute_tool):
+        mock_execute_tool.return_value = (0, self.temp_file.name)
         execute_pylint(self.project, self.mock_logger)
-        return execute_tool
+        return mock_execute_tool
