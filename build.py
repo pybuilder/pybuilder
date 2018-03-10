32dbbd54b5756130b2c862378568850907011743
Valentin Haenel <valentin@haenel.co>
2018-03-10 15:48:53 +0100

    bump to next dev version

--- build.py
+++ build.py
@@ -76,7 +76,7 @@ authors = [Author("Alexander Metzner", "alexander.metzner@gmail.com"),
            ]
 url = "http://pybuilder.github.io"
 license = "Apache License"
-version = "0.11.12"
+version = "0.11.13-dev"
 
 requires_python = ">=2.6,!=3.0,!=3.1,!=3.2,<3.7"
 
