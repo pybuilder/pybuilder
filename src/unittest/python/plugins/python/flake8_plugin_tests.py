a028fe5935e73a914716323c8cd2af1b9ba047e6
Arcadiy Ivanov <arcadiy@ivanov.biz>
2018-03-11 21:56:33 -0400

    Setuptools ~=38.5 Twine ~= 1.10 Flake ~= 3.5 Coverage ~= 4.5

--- src/unittest/python/plugins/python/flake8_plugin_tests.py
+++ src/unittest/python/plugins/python/flake8_plugin_tests.py
@@ -12,7 +12,7 @@ class FlakePluginInitializationTests(TestCase):
     def test_should_set_dependency(self):
         mock_project = Mock(Project)
         initialize_flake8_plugin(mock_project)
-        mock_project.plugin_depends_on.assert_called_with("flake8", "~=3.4")
+        mock_project.plugin_depends_on.assert_called_with("flake8", "~=3.5")
 
     def test_should_leave_user_specified_properties_when_initializing_plugin(self):
 
