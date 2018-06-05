469e4a3cf29d2d25065191e86a1227c94b10aa4f
Arcadiy Ivanov <arcadiy@ivanov.biz>
2018-06-05 07:22:05 -0400

    Setuptools 39.x

--- build.py
+++ build.py
@@ -96,7 +96,7 @@ def initialize(project):
 
     project.depends_on("tblib")
     project.depends_on("pip", "~=9.0")
-    project.depends_on("setuptools", "~=39.0.0")
+    project.depends_on("setuptools", "~=39.0")
     project.depends_on("wheel", "~=0.31")
     project.depends_on("tailer", "~=0.4")
 
