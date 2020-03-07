d2557162db6c3047994a0704ec3e7931e2884579
Arcadiy Ivanov <arcadiy@ivanov.biz>
2020-03-06 20:52:40 -0500

    Fix install dependencies not having a prepare and not creating target directories
    fixes #658

--- build.py
+++ build.py
@@ -33,6 +33,7 @@ bootstrap()
 use_plugin("python.core")
 use_plugin("python.pytddmon")
 use_plugin("python.distutils")
+use_plugin("python.install_dependencies")
 
 use_plugin("copy_resources")
 use_plugin("filter_resources")
