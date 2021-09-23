411dacc538800843ae7a05432dea35d28c1ba3b5
Arcadiy Ivanov <arcadiy@ivanov.biz>
2021-09-23 03:09:14 -0400

    Update deploy token Update supported Pythons

--- build.py
+++ build.py
@@ -177,6 +177,7 @@ def initialize(project):
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
