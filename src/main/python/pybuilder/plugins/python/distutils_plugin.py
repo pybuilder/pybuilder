10c9caa278e5e99e1f27cabf7181ea1e68ee5aef
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-02-21 09:10:55 -0500

    Add `eager_update` option to dependencies (#881)
    * Add `eager_update` option to dependencies
    
    Current functionality remains as is by default
    
    `eager_update=True` forces dependency to always update on all installs
    
    `eager_update=False` disables dependency updates even when version spec is non-exact
    
    * Disable Python 3.8 Homebrew

--- src/main/python/pybuilder/plugins/python/distutils_plugin.py
+++ src/main/python/pybuilder/plugins/python/distutils_plugin.py
@@ -114,9 +114,9 @@ def as_str(value):
 @init
 def initialize_distutils_plugin(project):
     project.plugin_depends_on("pypandoc", "~=1.4")
-    project.plugin_depends_on("setuptools", ">=38.6.0")
     project.plugin_depends_on("twine", ">=1.15.0")
-    project.plugin_depends_on("wheel", ">=0.34.0")
+    project.plugin_depends_on("setuptools", ">=38.6.0", eager_update=False)
+    project.plugin_depends_on("wheel", ">=0.34.0", eager_update=False)
 
     project.set_property_if_unset("distutils_commands", ["sdist", "bdist_wheel"])
     project.set_property_if_unset("distutils_command_options", None)
