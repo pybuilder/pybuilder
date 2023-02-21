9672cfdad1cd4c69e3dcbf4b7530302e302f8ec8
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-02-21 18:18:45 -0500

    Prepare 0.13.10.dev [skip ci] (#883)

--- build.py
+++ build.py
@@ -87,7 +87,7 @@ urls = {"Bug Tracker": "https://github.com/pybuilder/pybuilder/issues",
         "Twitter": "https://twitter.com/pybuilder_",
         }
 license = "Apache License, Version 2.0"
-version = "0.13.9"
+version = "0.13.10.dev"
 
 requires_python = ">=3.7"
 
